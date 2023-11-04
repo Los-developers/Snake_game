1fa00293ae66e56ddc6b2840e89a6ea08b8050dc
Frosmin <simonabastomart@gmail.com>
2023-11-04 08:11:28 -0400

    adding the database with its insert and view methods

--- src/main/java/DataBaseScore.java
+++ src/main/java/DataBaseScore.java
@@ -1,72 +1,84 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DataBaseScore {
-    // aqui la implementacion de una BD para el Score
 
+
+
+    // aqui la implementacion de una BD para el Score
     public static Connection getConexion() {
-        String connectionUrl = "jdbc:sqlserver://DESKTOP-ITA86RD\\SQLEXPRESS;databaseName=snake_DB;user=sa;password=12345;";
+        String connectionUrl = "jdbc:mysql://localhost:3306/snake?serverTimezone=UTC";
+        String username = "root";
+        String password = "";
 
         try {
-            Connection con = DriverManager.getConnection(connectionUrl);
+            Connection con = DriverManager.getConnection(connectionUrl, username, password);
             return con;
         } catch (SQLException ex) {
-            System.out.println(ex.toString());
+            System.out.println("\u001B[31m" + "El servidor no esta conectado. D:" + "\u001B[0m" + ex.toString());
             return null;
         }
     }
 
-    public static void insertarPuntaje(Connection conexion, String nombre, int puntaje) {
-        String insercion = "INSERT INTO score (NOMBRE, SCORE) VALUES (?, ?)";
-        try {
-            PreparedStatement statement = conexion.prepareStatement(insercion);
-            statement.setString(1, nombre);
-            statement.setInt(2, puntaje);
-            statement.executeUpdate();
-            System.out.println("Puntaje añadido: " + nombre + " - " + puntaje);
-        } catch (SQLException e) {
-            e.printStackTrace();
+    public static void insertarScore(Connection con, String ID_player, String nombre, int puntuacion) {
+        if (con != null) {
+            try {
+                String insertQuery = "INSERT INTO score (ID_player, nombre, puntuacion) VALUES (?, ?, ?)";
+                PreparedStatement preparedStatement = con.prepareStatement(insertQuery);
+                preparedStatement.setString(1, ID_player);
+                preparedStatement.setString(2, nombre);
+                preparedStatement.setInt(3, puntuacion);
+
+                int rowsAffected = preparedStatement.executeUpdate();
+
+                if (rowsAffected > 0) {
+                    System.out.println("\u001B[32m"+"Registro insertado con éxito."+"\u001B[0m");
+                } else {
+                    System.out.println("\u001B[31m" +"No se pudo insertar el registro."  + "\u001B[0m");
+                }
+
+                preparedStatement.close();
+            } catch (SQLException ex) {
+                System.out.println("\u001B[31m" + "Hay algun error en las  tablas o en la BD: " + "\u001B[0m" + ex.toString());
+            }
+        } else {
+            System.out.println("\u001B[31m" + "El servidor no esta conectado. D:" + "\u001B[0m");
         }
     }
 
 
-    public static void verPuntajes(Connection conexion) {
-        String consulta = "SELECT * FROM score";
-        try {
-            PreparedStatement statement = conexion.prepareStatement(consulta);
-            ResultSet resultSet = statement.executeQuery();
+    public static void mostrarScores(Connection con) {
+        if (con != null) {
+            try {
+                Statement statement = con.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM score");
 
-            while (resultSet.next()) {
-                int id = resultSet.getInt("ID");
-                String nombre = resultSet.getString("NOMBRE");
-                int puntaje = resultSet.getInt("SCORE");
-                System.out.println("ID: " + id + ", Nombre: " + nombre + ", Puntaje: " + puntaje);
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("ID_player") + " | " + resultSet.getString("nombre") + " | " + resultSet.getString("puntuacion"));
+                }
+
+                // Cerramos las conexiones a la base de datos
+                statement.close();
+                resultSet.close();
+            } catch (SQLException ex) {
+                System.out.println("\u001B[31m" + "Hay algun error en las  tablas o en la BD: " + "\u001B[0m" + ex.toString());
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } else {
+            System.out.println("\u001B[31m" + "El servidor no esta conectado. D:" + "\u001B[0m");
+
         }
     }
 
     public static void main(String[] args) {
-        Connection conexion = getConexion();
-
-     
-       insertarPuntaje(conexion, "Victor", 2000);
-
-     
-        System.out.println("Puntajes existentes:");
-        verPuntajes(conexion);
-
-       
-        try {
-            conexion.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+        Connection con = getConexion();
+        if (con != null) {
+            // Llama al método para insertar un nuevo registro
+            insertarScore(con, "004", "pepe", 120);
+            // Llama al método para mostrar los registros
+            mostrarScores(con);
         }
-
-        // alt + shift + f
     }
 }
+
+
+
+
--- src/main/java/Meal.java
+++ src/main/java/Meal.java
@@ -3,4 +3,26 @@ public class Meal {
     public int suma (int x, int y){
         return  x+y;
     }
+
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10 ; i++) {
+            for (int j = 0; j < 10 ; j++) {
+                for (int k = 0; k <10 ; k++) {
+                    for (int l = 0; l < 10 ; l++) {
+                        for (int m = 0; m < 10; m++) {
+                            for (int n = 0; n < 10 ; n++) {
+                                System.out.println(j);
+                                System.out.print(i);
+                                System.out.print(k);
+                                System.out.print(l);
+                                System.out.print(m);
+                                System.out.print(n);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
